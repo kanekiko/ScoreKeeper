8da81377378e5234609809b824ff78f478ef573b
Sara Jourdan <sara.jourdanoff@icloud.com>
2025-05-26 07:02:43 +0200

    feat(index.js): add game variable

--- js/index.js
+++ js/index.js
@@ -6,3 +6,8 @@ const scoreUn = document.querySelector(".score-1");
 const scoreDeux = document.querySelector(".score-2");
 const boutonReinitialiser = document.querySelector(".btn-reset");
 const saisieCible = document.querySelector("#cible");
+
+let currentScoreOne = 0;
+let currentScoreTwo = 0;
+let targetScore = 7;
+let isGameOver = false;
