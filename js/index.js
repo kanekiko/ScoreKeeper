604fb3261072b3ff77e25c5b8f38235bdd43e597
Sara Jourdan <sara.jourdanoff@icloud.com>
2025-05-26 08:02:00 +0200

    feat(index.js): add updateScore disable and enable button

--- js/index.js
+++ js/index.js
@@ -11,3 +11,26 @@ let currentScoreOne = 0;
 let currentScoreTwo = 0;
 let targetScore = 7;
 let isGameOver = false;
+
+const updateScoreDisplay = () => {
+  scoreUn.textContent = currentScoreOne;
+  scoreDeux.textContent = currentScoreTwo;
+};
+
+const disableButtons = (winnerDisplay, loserDisplay) => {
+  boutonIncrementUn.classList.add("disabled");
+  boutonIncrementDeux.classList.add("disabled");
+  winnerDisplay.classList.add("gagnant");
+  loserDisplay.classList.add("perdant");
+  isGameOver = true;
+};
+
+const enableButtons = () => {
+  boutonIncrementUn.classList.remove("disabled");
+  boutonIncrementDeux.classList.remove("disabled");
+  scoreUn.classList.remove("gagnant");
+  scoreDeux.classList.remove("perdant");
+  scoreUn.classList.remove("perdant");
+  scoreDeux.classList.remove("gagnant");
+  isGameOver = false;
+};
