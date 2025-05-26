42ce85805f9c0f1b30864f0d82c53950fab7acb2
Sara Jourdan <sara.jourdanoff@icloud.com>
2025-05-26 06:59:54 +0200

    feat(index.js): add selector

---
+++ js/index.js
@@ -0,0 +1,8 @@
+"use strict"
+
+const boutonIncrementUn = document.querySelector(".btn-inc-1");
+const boutonIncrementDeux = document.querySelector(".btn-inc-2");
+const scoreUn = document.querySelector(".score-1");
+const scoreDeux = document.querySelector(".score-2");
+const boutonReinitialiser = document.querySelector(".btn-reset");
+const saisieCible = document.querySelector("#cible");
